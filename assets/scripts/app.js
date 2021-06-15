1f237f6c3cf0f76a868f691c96b5dc80b290cdaa
unknown <msayem089@gmail.com>
2021-06-15 23:05:32 +0600

    main pages are completed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -47,3 +47,70 @@ searchBtn.forEach((eachBtn) => {
 closeSearchBtn.onclick = () => {
   searchElement.classList.remove('active')
 }
+
+// Call the dropdowns via JavaScript
+const dropdownElementList = [].slice.call(
+  document.querySelectorAll('.dropdown-toggle')
+)
+const dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
+
+//show search results
+const searchInput = document.querySelector('#game-search')
+const searchResult = document.querySelector('.search-details')
+
+searchInput?.addEventListener('focus', () => {
+  searchResult.classList.add('active')
+})
+
+//notification dropdown
+const notificationBtn = document.querySelector('.notification')
+const notificationDropdown = document.querySelector('.notification-dropdown')
+notificationBtn?.addEventListener('click', () => {
+  notificationDropdown.classList.toggle('active')
+})
+
+//add listing page
+const searchLists = document.querySelectorAll('.search-details li > div')
+const resetBtn = document.querySelector('.reset-btn')
+const searchSectionEl = document.querySelector('.search-section')
+const selectedSectionEl = document.querySelector('.selected-section')
+searchLists?.forEach((searchItem) => {
+  searchItem.addEventListener('click', () => {
+    searchResult.classList.remove('active')
+    let scrollHeight = selectedSectionEl.scrollHeight
+    searchSectionEl.style.cssText = 'height: 0px; overflow: hidden'
+    selectedSectionEl.style.height = scrollHeight + 'px'
+  })
+})
+resetBtn?.addEventListener('click', () => {
+  let scrollHeight = searchSectionEl.scrollHeight
+  console.log('reset')
+  selectedSectionEl.style.height = '0px'
+  searchSectionEl.style.cssText = `height: ${scrollHeight}px; overflow: hidden`
+  setTimeout(() => {
+    searchSectionEl.style.overflow = 'unset'
+  }, 300)
+})
+
+//add game page dropdown
+const changingBackground = document.querySelector('.system')
+const changingText = document.querySelector('.changing-text')
+const systemItems = document.querySelectorAll('.system-item')
+const systemDropdown = document.querySelector('.system-dropdown')
+const searching = document.querySelector('.searching')
+const searchGame = document.querySelector('.search-game-btn')
+
+systemItems.forEach((systemItem) => {
+  systemItem.addEventListener('click', () => {
+    changingText.innerHTML = systemItem?.innerText || ''
+    changingBackground.style.background = systemItem.getAttribute('data-color')
+  })
+})
+
+searchGame.addEventListener('click', () => {
+  searchGame.innerHTML = '<i class="fa fa-spinner fa-pulse fa-fw"></i>'
+  systemDropdown.classList.add('d-none')
+  searching.classList.remove('d-none')
+})
